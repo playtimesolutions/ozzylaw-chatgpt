aaf85211c80029dadbb39b92dcbb8bdc990c6720
Michael John Peña <michael.pena@playtimesolutions.com.au>
2024-01-22 09:36:16 +1100

    more branding elements

--- frontend/src/components/common/Button.tsx
+++ frontend/src/components/common/Button.tsx
@@ -1,70 +1,68 @@
 import { CommandBarButton, DefaultButton, IButtonProps, IButtonStyles, ICommandBarStyles } from "@fluentui/react";
 
 interface ShareButtonProps extends IButtonProps {
-    onClick: () => void;
-  }
+  onClick: () => void;
+}
 
-export const ShareButton: React.FC<ShareButtonProps> = ({onClick}) => {
-    const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
-        root: {
-          width: 86,
-          height: 32,
-          borderRadius: 4,
-          background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #0F6CBD 33.63%, #2D87C3 70.31%, #8DDDD8 100%)',
-        //   position: 'absolute',
-        //   right: 20,
-          padding: '5px 12px',
-          marginRight: '20px'
-        },
-        icon: {
-          color: '#FFFFFF',
-        },
-        rootHovered: {
-          background: 'linear-gradient(135deg, #0F6CBD 0%, #2D87C3 51.04%, #8DDDD8 100%)',
-        },
-        label: {
-          fontWeight: 600,
-          fontSize: 14,
-          lineHeight: '20px',
-          color: '#FFFFFF',
-        },
-      };
+export const ShareButton: React.FC<ShareButtonProps> = ({ onClick }) => {
+  const shareButtonStyles: ICommandBarStyles & IButtonStyles = {
+    root: {
+      width: 86,
+      height: 32,
+      borderRadius: 4,
+      background: 'radial-gradient(109.81% 107.82% at 100.1% 90.19%, #008000 33.63%, #FFD700 70.31%, #FFD700 100%)',
+      padding: '5px 12px',
+      marginRight: '20px'
+    },
+    icon: {
+      color: '#FFFFFF',
+    },
+    rootHovered: {
+      background: 'linear-gradient(135deg, #0F6CBD 0%, #2D87C3 51.04%, #8DDDD8 100%)',
+    },
+    label: {
+      fontWeight: 600,
+      fontSize: 14,
+      lineHeight: '20px',
+      color: '#FFFFFF',
+    },
+  };
 
-      return (
-        <CommandBarButton
-                styles={shareButtonStyles}
-                iconProps={{ iconName: 'Share' }}
-                onClick={onClick}
-                text="Share"
-        />
-      )
+  return (
+    <CommandBarButton
+      styles={shareButtonStyles}
+      iconProps={{ iconName: 'Share' }}
+      onClick={onClick}
+      text="Share"
+    />
+  )
 }
 
 interface HistoryButtonProps extends IButtonProps {
-    onClick: () => void;
-    text: string;
-  }
+  onClick: () => void;
+  text: string;
+}
 
-export const HistoryButton: React.FC<HistoryButtonProps> = ({onClick, text}) => {
-    const historyButtonStyles: ICommandBarStyles & IButtonStyles = {
-        root: {
-            width: '180px',
-            border: `1px solid #D1D1D1`,
-          },
-          rootHovered: {
-            border: `1px solid #D1D1D1`,
-          },
-          rootPressed: {
-            border: `1px solid #D1D1D1`,
-          },
-      };
+export const HistoryButton: React.FC<HistoryButtonProps> = ({ onClick, text }) => {
+  const historyButtonStyles: ICommandBarStyles & IButtonStyles = {
+    root: {
+      width: '180px',
+      border: `1px solid #D1D1D1`,
+    },
+    rootHovered: {
+      border: `1px solid #D1D1D1`,
+    },
+    rootPressed: {
+      border: `1px solid #D1D1D1`,
+    },
+  };
 
-      return (
-        <DefaultButton
-            text={text}
-            iconProps={{ iconName: 'History' }}
-            onClick={onClick}
-            styles={historyButtonStyles}
-        />
-      )
+  return (
+    <DefaultButton
+      text={text}
+      iconProps={{ iconName: 'History' }}
+      onClick={onClick}
+      styles={historyButtonStyles}
+    />
+  )
 }
--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -591,8 +591,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Your Ozzy Legal Copilot</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Ask anything about Australian law and get instant answers and references.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px" }} role="log">
